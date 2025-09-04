4f6ac6ad5d07b2d004436a246a170628d5a9e196
baotran27525 <baotran.31241028457@st.ueh.edu.vn>
2025-09-04 23:13:35 +0700

    baitap0409

--- csharp-assignment-hello-world/csharp-assignment-hello-world.Tests/obj/Release/net9.0/csharp-assignment-hello-world.Tests.AssemblyInfo.cs
+++ csharp-assignment-hello-world/csharp-assignment-hello-world.Tests/obj/Release/net9.0/csharp-assignment-hello-world.Tests.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp-assignment-hello-world.Tests")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2da0864b986bf5ad52182032fa481cb28eaadf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp-assignment-hello-world.Tests")]
